c48cdbaf823b89d7eda1bb3eefe45df259983298
XDGFX <16356208+XDGFX@users.noreply.github.com>
2019-01-06 11:14:14 +0000

    Improved path for veriables file
    Now the script looks for variables.sh in the directory the script was executed in (had some errors previously where it couldn't find the file)

--- copy_playlists.sh
+++ copy_playlists.sh
@@ -1,5 +1,6 @@
 # Copies playlists from one directory to a folder, as long as the file is a .m3u
-source variables.sh
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPTDIR/variables.sh;
 
 rm -f $plex_playlists/* && cd $playlists && find -iname '*.m3u' -exec cp -prv {} "$plex_playlists" ";"
 
