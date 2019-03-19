337003daa37351ec5f76fb3df08b5cec72e66ffe
XDGFX <16356208+XDGFX@users.noreply.github.com>
2019-03-19 20:40:12 +0000

    Delete diredit_playlists.sh

--- diredit_playlists.sh
+++
@@ -1,7 +0,0 @@
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $SCRIPTDIR/variables.sh;
-
-cd $plex_playlists && sed -i 's|\\\\server\\Uluru|D:|g' *
-# This many backslashes are needed to 'escape' each initial backslash. The things to change here are:
-# '\\\\server\\Uluru' is the path you want to remove from the .m3u text
-# ... to replace it with 'D:'
