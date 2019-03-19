fa5f62f4cc10a2c40fbf9c84724f372ac1cb446b
XDGFX <16356208+XDGFX@users.noreply.github.com>
2019-03-19 20:39:48 +0000

    Delete copy_playlists.sh

--- copy_playlists.sh
+++
@@ -1,6 +0,0 @@
-# Copies playlists from one directory to a folder, as long as the file is a .m3u
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $SCRIPTDIR/variables.sh;
-
-rm -f $plex_playlists/* && cd $playlists && find -iname '*.m3u' -exec cp -prv {} "$plex_playlists" ";"
-
